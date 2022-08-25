b8f82819637f5707c773c2fdaab5ac1219577d07
Rj123 <95161695+Raynould-Joseph@users.noreply.github.com>
2022-08-25 17:59:29 +0530

    9/8/2022

--- app/src/main/java/com/example/spacebook/facts_about_space.java
+++ app/src/main/java/com/example/spacebook/facts_about_space.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class facts_about_space extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facts_about_space);
+
     }
 }
